d1171a61bd94e9f41e20c1607f01eda0beb5134a
Mateus Correia <mateus.correia@outlook.com>
2022-04-12 06:38:08 -0300

    a new commit in my project

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -34,8 +34,8 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const addProduct = async (productId: number) => {
     try {
-      const updateCart = [...cart];
-      const productExists = updateCart.find(product => product.id === productId);
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
     
       const stock = await api.get(`/stock/${productId}`);
 
@@ -57,11 +57,11 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
           ...product.data,
           amount: 1
         }
-        updateCart.push(newProduct);
+        updatedCart.push(newProduct);
       }
 
-      setCart(updateCart);
-      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updateCart))
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
     } catch {
       toast.error('Erro na adição do produto');
     }
@@ -69,9 +69,18 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const productIndex = updatedCart.findIndex(product => product.id === productId);
+
+      if (productIndex >= 0) {
+        updatedCart.splice(productIndex, 1);
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+      } else {
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
